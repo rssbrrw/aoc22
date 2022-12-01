b8fab79173cc9582675916b6db477f2b7722d905
rssbrrw <rssbrrw@gmail.com>
2022-12-01 17:25:47 +0000

    Dynamic input

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,13 @@
 module Main (main) where
 
+
+import System.Environment (getArgs)
+
 import One
 
 main :: IO ()
 main = do
-    input <- lines <$> readFile "input.txt"
+    dayNo <- head <$> getArgs
+    input <- lines <$> readFile ("input" ++ dayNo ++ ".txt")
     print $ solutionOne input
     print $ solutionTwo input
